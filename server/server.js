1a48a69e88240dbb3a53df1c5a2082be689004d5
Ritesh-0909 <ry0103378@gmail.com>
2026-09-04 14:25:59 +0530

    Separate backend from frontend deployment

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const path = require('path');
+
 
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
@@ -36,19 +36,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/auth', authRoutes);
 app.use('/api/ai', aiRoutes);
 
-// Absolute path to client build folder
-const __dirnamePath = path.resolve();
-const clientBuildPath = path.join(__dirnamePath, '..', 'client', 'dist');
-
-// Serve static files
-app.use(express.static(clientBuildPath));
-
-// For any route not starting with /api, send index.html
-app.get('/{*splat}', (req, res) => {
-    if (!req.path.startsWith('/api')) {
-        res.sendFile(path.join(clientBuildPath, 'index.html'));
-    }
-});
 
 
 app.use((err, req, res, next) => {
